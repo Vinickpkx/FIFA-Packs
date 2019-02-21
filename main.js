4000d55b84032b5265a089508ec29e60108193d3
Unknown <cristianraffi@bol.com.br>
2019-02-21 14:42:55 -0300

    1º commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function foto() {
+
+    document.getElementById("foto").style.opacity = 0;
+
+    var timer = setInterval(function() {
+        var calculo = Math.floor(Math.random() * 6);
+        switch (calculo) {
+            case 1:
+                document.getElementById("foto").src = "Players/PL1.png";
+                break;
+
+            case 2:
+                document.getElementById("foto").src = "Players/PL2.png";
+                break;
+
+            case 3:
+                document.getElementById("foto").src = "Players/PL3.png";
+                break;
+
+            case 4:
+                document.getElementById("foto").src = "Players/PL4.png";
+                break;
+
+            case 5:
+                document.getElementById("foto").src = "Players/PL5.png";
+                break;
+
+            case 6:
+                document.getElementById("foto").src = "Players/PL6.png";
+                break;
+        }
+        clearInterval(timer);
+        document.getElementById("foto").style.opacity = 100;
+
+    }, 1800);
+}
