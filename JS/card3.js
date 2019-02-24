fd9af8286e4a1681e39947b38ff9b1c817c83909
Unknown <cristianraffi@bol.com.br>
2019-02-24 17:03:44 -0300

    Selection function JS slider

---
+++ JS/card3.js
@@ -0,0 +1,17 @@
+function op1() {
+    var elemento1 = document.getElementById("op1").style.backgroundColor = "rgba(241, 241, 241, 0.712)";
+    var elemento2 = document.getElementById("op2").style.backgroundColor = "#fff";
+    var elemento3 = document.getElementById("op3").style.backgroundColor = "#fff";
+}
+
+function op2() {
+    var elemento1 = document.getElementById("op1").style.backgroundColor = "#fff";
+    var elemento2 = document.getElementById("op2").style.backgroundColor = "rgba(241, 241, 241, 0.712)";
+    var elemento3 = document.getElementById("op3").style.backgroundColor = "#fff";
+}
+
+function op3() {
+    var elemento1 = document.getElementById("op1").style.backgroundColor = "#fff";
+    var elemento2 = document.getElementById("op2").style.backgroundColor = "#fff";
+    var elemento3 = document.getElementById("op3").style.backgroundColor = "rgba(241, 241, 241, 0.712)";
+}
